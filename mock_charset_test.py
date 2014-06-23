d129b0ad1a0836e2b00cff4e4d106eed8f85a4ba
Eluvatar <drstadler@gmail.com>
2014-06-23 13:17:59 -0400

    add region pass-through test with eacute

--- mock_charset_test.py
+++ mock_charset_test.py
@@ -3,4 +3,7 @@ api.user_agent = 'charset test by Eluvatar'
 logging.basicConfig()
 xml = api.request({'q':'happenings','sinceid':'7999000'},True)
 events = xml.find("HAPPENINGS").findall("EVENT")
-print filter(lambda x: x.get('id') == "7999047", events)[0].find("TEXT").text 
+print filter(lambda x: x.get('id') == "7999047", events)[0].find("TEXT").text
+xml = api.request({'q':'factbook','region':'swiss_confederation'},True)
+print xml.find("FACTBOOK").text
+ 
