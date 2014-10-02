f7bee7d68d6f07dc22564145e1053d8c55eaebbf
Eluvatar <drstadler@gmail.com>
2014-10-01 22:40:40 -0400

    fixing old typo....

--- api.py
+++ api.py
@@ -36,7 +36,7 @@ import io
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-def request(query,log=False):
+def request(query,header=False,log=False):
     """ requests information from the NS API using trawler
         defaults to using version 5 (the latest when this code was written) """
     if 'v' in query:
@@ -54,14 +54,17 @@ def request(query,log=False):
     if( res.result == 200 ):
         try:
             utfstr = unicode(res.read(),'windows-1252')
-        except UnicodeDecodeError err:
+        except UnicodeDecodeError as err:
             logger.error("GET %s?%s -> %d:", path, querystr, res.result)
             logger.error("%s",res.read())
+        if 'a' in query and query['a'] == 'sendTG':
+            # Why does this have to be plaintext and not XML? :(
+            return utfstr
         xmlstr = utfstr.encode('ascii','xmlcharrefreplace')
         try:
             return ET.fromstring(xmlstr)
         except (EE,PE):
-            __handle_ee(querystr, res)
+            _handle_parse_error(querystr, res)
             raise
     elif( res.result == 404 ):
         if 'nation' in query:
@@ -87,7 +90,7 @@ class ApiError(Exception):
     def __str__(self):
         return repr(self.value)
 
-def __handle_ee(query,res):
+def _handle_parse_error(query,res):
     logger.error("api.request of %s failed to parse",query)
     if logger.isEnabledFor(logging.DEBUG):
         logger.debug("---begin---")
